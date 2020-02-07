43aba7af88374044ba21d3174c91dd8e390b4c4f
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-02-07 21:27:21 +0000

    Create user schema

---
+++ backend/src/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
