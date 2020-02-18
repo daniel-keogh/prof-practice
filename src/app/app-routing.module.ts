cee9d8cda30bcdf65a607b9d19c283447e07c2a5
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-02-18 12:23:27 +0000

    Nest profile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,29 +10,29 @@ const routes: Routes = [
   },
   {
     path: 'start-page',
-    loadChildren: () => import('./pages/start-page/start-page.module').then( m => m.StartPagePageModule)
+    loadChildren: () => import('./pages/start-page/start-page.module').then(m => m.StartPagePageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
     // canActivate: [AuthGuard]
   },
   {
     path: 'settings',
-    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule),
+    loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsPageModule),
     // canActivate: [AuthGuard]
   },
   {
-    path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
+    path: 'settings/profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
     // canActivate: [AuthGuard]
   }
 ];
