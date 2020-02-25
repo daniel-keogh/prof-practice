206b0bcdd148667cf99cb85b88b52f9ab7e5885d
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-02-25 11:01:48 +0000

    Fix UnhandledPromiseRejectionWarnings

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -7,68 +7,69 @@ const User = require('../models/user');
 exports.registerUser = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(422).json({ 'errors': errors.array() });
-    } else {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+        return res.status(422).json({ 'errors': errors.array() });
+    }
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save()
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(400).json(err);
         });
 
-        user.save()
-            .then(user => {
-                res.status(201).json(user);
-            })
-            .catch(err => {
-                res.status(400).json(err);
-            });
-    }
 };
 
 exports.login = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(422).json({ 'errors': errors.array() });
-    } else {
-        let foundUser;
-        User.findOne({ email: req.body.email })
-            .select('+password')
-            .exec()
-            .then(user => {
-                if (!user) {
-                    const error = new Error();
-                    error.status = 401;
-                    throw error;
-                } else {
-                    foundUser = user;
-                    return bcrypt.compare(req.body.password, foundUser.password);
-                }
-            })
-            .then(isMatch => {
-                if (isMatch) {
-                    res.status(200).json({
-                        'msg': 'User logged in successfully',
-                        'token': jwt.sign({
-                            _id: foundUser._id,
-                            email: foundUser.email,
-                            name: foundUser.name,
-                            registered_since: foundUser.registered_since
-                        }, secret)
-                    });
-                } else {
-                    const error = new Error();
-                    error.status = 401;
-                    throw error;
-                }
-            })
-            .catch(err => {
-                if (err.status === 401) {
-                    res.status(401).json({ 'msg': 'User email or password is incorrect' });
-                } else {
-                    res.status(400).json(err);
-                }
-            });
+        return res.status(422).json({ 'errors': errors.array() });
     }
+
+    let foundUser;
+    User.findOne({ email: req.body.email })
+        .select('+password')
+        .exec()
+        .then(user => {
+            if (!user) {
+                const error = new Error();
+                error.status = 401;
+                throw error;
+            } else {
+                foundUser = user;
+                return bcrypt.compare(req.body.password, foundUser.password);
+            }
+        })
+        .then(isMatch => {
+            if (isMatch) {
+                res.status(200).json({
+                    'msg': 'User logged in successfully',
+                    'token': jwt.sign({
+                        _id: foundUser._id,
+                        email: foundUser.email,
+                        name: foundUser.name,
+                        registered_since: foundUser.registered_since
+                    }, secret)
+                });
+            } else {
+                const error = new Error();
+                error.status = 401;
+                throw error;
+            }
+        })
+        .catch(err => {
+            if (err.status === 401) {
+                res.status(401).json({ 'msg': 'User email or password is incorrect' });
+            } else {
+                res.status(400).json(err);
+            }
+        });
 };
 
 exports.passwordReset = (req, res) => {
--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -18,26 +18,28 @@ exports.getUser = (req, res) => {
 exports.updateUser = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(422).json({ 'errors': errors.array() });
-    } else {
-        User.findById(req.params.id)
-            .then(user => {
-                if (!user) {
-                    res.status(404).json({ 'msg': 'User not found' });
-                } else {
-                    const { name, email } = req.body;
-
-                    user.name = name;
-                    user.email = email;
-
-                    return user.save();
-                }
-            }).then(user => {
-                res.status(200).json(user);
-            }).catch(err => {
-                res.status(500).json(err);
-            });
+        return res.status(422).json({ 'errors': errors.array() });
     }
+
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ 'msg': 'User not found' });
+            } else {
+                const { name, email } = req.body;
+
+                user.name = name;
+                user.email = email;
+
+                return user.save();
+            }
+        })
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 };
 
 exports.deleteUser = (req, res) => {
