dac273053b2654b49c07869ea8bed6a103e3177c
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-07 12:48:30 +0000

    Start working on adding Chart.js

--- src/app/pages/water/water.page.ts
+++ src/app/pages/water/water.page.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { StatsService } from './../../services/stats/stats.service';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-water',
   templateUrl: './water.page.html',
-  styleUrls: ['./water.page.scss'],
+  styleUrls: ['./water.page.scss']
 })
 export class WaterPage implements OnInit {
-
-  constructor() { }
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  constructor(private stats: StatsService) {}
 
   ngOnInit() {
+    const days = 7;
+    this.createChart(days);
   }
 
+  async createChart(days?: number, months?: number) {
+    // get the array of days with the amount of water drank on each day & generate the chart
+    this.stats.generateChart({
+      canvas: this.canvas
+    });
+  }
+
+  segmentChanged(ev: any) {
+    let months: number;
+    let days: number;
+
+    enum Months {
+      month = 1,
+      sixMonths = 6,
+      year = 12
+    }
+
+    switch (ev.detail.value) {
+      case 'week':
+        days = 7;
+        break;
+      case 'month':
+      case 'year':
+        months = Months[ev.detail.value as string];
+        break;
+      default:
+        break;
+    }
+    this.createChart(days, months);
+  }
 }
